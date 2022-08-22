f0005dd999b405fe2e033dd714aee72b7647df91
Viktor Mehl <vikt2553@mail.hansenberg.dk>
2022-08-22 12:50:14 +0200

    story moved to helloworld, stuff fixed

--- 0_HelloWorld/Program.cs
+++ 0_HelloWorld/Program.cs
@@ -4,6 +4,46 @@ Console.ForegroundColor = ConsoleColor.DarkGreen;
 Console.WindowHeight = 40;
 
 //Presentation
-Console.WriteLine("Hello Chosen One");
-Console.WriteLine("Where is our next adventure?");
-string answer = Console.ReadLine();
+Console.WriteLine("Did you ever hear the tragedy of darth plagueius the wise?");
+
+string firstanswer = Console.ReadLine();
+bool Darth = false;
+if (firstanswer == "no")
+{
+    Darth = true;
+}
+else
+{
+    Darth = false;
+}
+bool cool = false;
+if (Darth == true)
+{
+    Console.WriteLine("I thought not, its not a story the jedi would tell you");
+    Thread.Sleep(500);
+    Console.WriteLine("Its a Sith legend.");
+    Thread.Sleep(500);
+    Console.WriteLine("Darth Plagueius was a sith lord so powerful he could save others from dying.");
+    Thread.Sleep(500);
+    Console.WriteLine("do you want to know more?");
+    string secondAnswer = Console.ReadLine();
+    if (secondAnswer == "yes")
+    {
+        cool = true;
+    }
+}
+
+if (cool == true)
+{
+    Console.WriteLine("bla bla bla");
+    Thread.Sleep(500);
+    Console.WriteLine("(after what seems like ages you grow tired of hearing his constant blabbering)");
+    Thread.Sleep(500);
+    Console.WriteLine("(You decide to threaten him so that he shuts up)");
+
+}
+else
+{
+    Console.WriteLine("f you then");
+}
+Console.ReadLine();
--- 2_Conditionals/Program.cs
+++ 2_Conditionals/Program.cs
@@ -4,47 +4,47 @@ Console.WriteLine("Look at the source code to learn more about conditionals!");
 string StringInputFromConsole = Console.ReadLine();
 
 bool BooleanInput = false;
-if(StringInputFromConsole == "haps" || StringInputFromConsole == "snaps")
+if (StringInputFromConsole == "haps" || StringInputFromConsole == "snaps")
 {
-  BooleanInput = true;
+    BooleanInput = true;
 }
 
-if(BooleanInput == true)
+if (BooleanInput == true)
 {
-  Console.WriteLine("Conditions are easy!"); 
+    Console.WriteLine("Conditions are easy!");
 }
 else
 {
-  Console.WriteLine("Read the source code for this program to learn more!"); 
+    Console.WriteLine("Read the source code for this program to learn more!");
 }
 
-if(2 > 5)
+if (2 > 5)
 {
-  Console.WriteLine("2 was greater than 5");
+    Console.WriteLine("2 was greater than 5");
 }
-else if(2+2 == 5)
+else if (2 + 2 == 5)
 {
-  Console.WriteLine("2+2 was equal to 5");
+    Console.WriteLine("2+2 was equal to 5");
 }
-else if(4 != 4)
+else if (4 != 4)
 {
-  Console.WriteLine("4 is NOT equal to 4");
+    Console.WriteLine("4 is NOT equal to 4");
 }
 else
 {
-  if(2*2 >= 5)
-  {
-      Console.WriteLine("2 times 2 is greater than or equal to 5");    
-  }
-  else if(true)
-  {
-    Console.WriteLine("true");
-  }
+    if (2 * 2 >= 5)
+    {
+        Console.WriteLine("2 times 2 is greater than or equal to 5");
+    }
+    else if (true)
+    {
+        Console.WriteLine("true");
+    }
 
-  if(false == false && true == true)
-  {
-    Console.WriteLine("false is equal to false AND true is equal to true");
-  }
+    if (false == false && true == true)
+    {
+        Console.WriteLine("false is equal to false AND true is equal to true");
+    }
 }
 
 
@@ -73,32 +73,32 @@ Console.WriteLine("Input a number between 1 and 7");
 string inputFromConsole = Console.ReadLine();
 int day = int.Parse(inputFromConsole);
 
-switch (day) 
+switch (day)
 {
-  case 1:
-    Console.WriteLine("Monday");
-    break;
-  case 2:
-    Console.WriteLine("Tuesday");
-    break;
-  case 3:
-    Console.WriteLine("Wednesday");
-    break;
-  case 4:
-    Console.WriteLine("Thursday");
-    break;
-  case 5:
-    Console.WriteLine("Friday");
-    break;
-  case 6:
-    Console.WriteLine("Saturday");
-    break;
-  case 7:
-    Console.WriteLine("Sunday");
-    break;
-  default:
-    Console.WriteLine("Mellem 1 og 7 din skovl!");
-    break;
+    case 1:
+        Console.WriteLine("Monday");
+        break;
+    case 2:
+        Console.WriteLine("Tuesday");
+        break;
+    case 3:
+        Console.WriteLine("Wednesday");
+        break;
+    case 4:
+        Console.WriteLine("Thursday");
+        break;
+    case 5:
+        Console.WriteLine("Friday");
+        break;
+    case 6:
+        Console.WriteLine("Saturday");
+        break;
+    case 7:
+        Console.WriteLine("Sunday");
+        break;
+    default:
+        Console.WriteLine("Mellem 1 og 7 din skovl!");
+        break;
 }
 Console.ReadLine();
 #endregion
