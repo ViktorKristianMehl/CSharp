02d2ddbd11d375f5f9fc770746f94742e561c249
Viktor Mehl <vikt2553@mail.hansenberg.dk>
2022-09-22 10:50:12 +0200

    Færdig
    fibbonacci + historie er nu færdig

--- 0_HelloWorld/Program.cs
+++ 0_HelloWorld/Program.cs
@@ -1,4 +1,5 @@
-﻿//Change appearance of Console
+﻿
+//Change appearance of Console
 Console.Title = "Matrix";
 Console.ForegroundColor = ConsoleColor.DarkGreen;
 Console.WindowHeight = 40;
@@ -42,8 +43,127 @@ if (cool == true)
     Console.WriteLine("(You decide to threaten him so that he shuts up)");
 
 }
+
 else
 {
     Console.WriteLine("f you then");
 }
+
+
+List<string> attacks = new List<string>();
+
+Console.WriteLine("Write 5 ways to threaten him");
+for (int i = 0; i < 5; i++)
+{
+    attacks.Add(Console.ReadLine());
+}
+
+Random attack = new Random();
+int randomAttack = attack.Next(0, 4);
+Console.WriteLine(attacks[randomAttack]);
+Thread.Sleep(500);
+switch (randomAttack)
+{
+
+    case 0:
+        Console.WriteLine("you threaten him with your " + attacks[0] + " and looks he confused");
+        break;
+
+    case 1:
+        Console.WriteLine("you threaten him with your " + attacks[1] + " and he laughs");
+        break;
+
+    case 2:
+        Console.WriteLine("you threaten him with your " + attacks[2] + " and you see him getting nervous and he leaves");
+        break;
+    case 3:
+        Console.WriteLine("you threaten him with your " + attacks[3] + " and he flees the scene");
+        break;
+    case 4:
+        Console.WriteLine("you threaten him with your " + attacks[4] + " and he ignores you");
+        break;
+}
+Thread.Sleep(500);
+if (randomAttack == 0 || randomAttack == 1 || randomAttack == 4)
+{
+
+    Console.WriteLine("you fail to make him go away. You will have to listen to his talking for eternity");
+}
+else
+{
+    Console.WriteLine("he flees the scene, now you are free");
+};
+
+bool loss = false;
+if (randomAttack == 0 || randomAttack == 1 || randomAttack == 4)
+{
+    loss = true;
+}
+Thread.Sleep(500);
+bool battle = false;
+
+if (loss == true)
+{
+    Console.WriteLine("threatening him didn't work. You will have to fight him");
+    string[] weapons = {
+    "Gun",
+    "Sword",
+    "Frying Pan",
+    "Fists",
+    "Bow"
+    };
+    Thread.Sleep(300);
+
+    Console.WriteLine("Which the following weapons do you choose for the battle?");
+
+    foreach (string item in weapons)
+    {
+        Console.WriteLine(item);
+    }
+    string weaponChoice = Console.ReadLine();
+    if (weaponChoice == "Gun" || weaponChoice == "gun")
+    {
+        battle = true;
+    }
+    else
+    {
+        battle = false;
+    }
+    string diceAnswer;
+    if (battle == true)
+    {
+        Console.WriteLine("He runs towards you with his lightsaber but you quickly pull out your gun and shoot him in the head. You win");
+    }
+    else if (battle == false)
+    {
+        Console.WriteLine("Your weapon has no effect on him as he dodges every try to him him");
+        Thread.Sleep(300);
+        Console.WriteLine("He moves towards out and cuts of your hand in one swing");
+        Thread.Sleep(300);
+        Console.WriteLine("you beg for mercy /n him: if you roll a 6 with this dice, i will let you live");
+        Thread.Sleep(500);
+        Console.WriteLine("Do you accept this challenge?");
+    }
+    diceAnswer = Console.ReadLine();
+    Random diceroll = new Random();
+    Thread.Sleep(500);
+    while (diceAnswer == "yes")
+    {
+        Console.WriteLine("you roll the dice");
+        int yourRoll = diceroll.Next(1, 6);
+        Console.WriteLine("you roll a " + yourRoll);
+        if (yourRoll == 6)
+        {
+            Console.WriteLine("You have been spared");
+            break;
+        }
+        else
+        {
+            Console.WriteLine("You lose and he kills you");
+            break;
+        }
+    }
+}
 Console.ReadLine();
+
+
--- 5_Assignment/Program.cs
+++ 5_Assignment/Program.cs
@@ -1,21 +1,24 @@
 ﻿//Un-comment below code and fix the issues
 //Push to github
-/*
 int RecursiveFibonacci(int first, int second, int count)
 {
     count--;
-    
-    
-    if(count == 0) {
-    
-    }else{
-        return RecursiveFibonacci(?, ?, ?);
+    int newnumber = first + second;
+    first = second;
+    second = newnumber;
+
+    if (count == 0)
+    {
+        return newnumber;
+    }
+    else
+    {
+        return RecursiveFibonacci(first, second, count);
     }
 }
 
-int lastFibo = RecursiveFibonacci(1,1,5);
+int lastFibo = RecursiveFibonacci(1, 1, 5);
 Console.WriteLine("Last fibo number was " + lastFibo);
-Console.ReadLine(); 
-*/
+Console.ReadLine();
 
 
